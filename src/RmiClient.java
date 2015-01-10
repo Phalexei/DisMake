fc7159bbfa2c4fb3b83363fd9a315b6893394e66
Tom Andre-Poyaud <tom.andre-poyaud@ensimag.imag.fr>
2015-01-10 15:13:40 +0100

    bit of doc

--- src/RmiClient.java
+++ src/RmiClient.java
@@ -1,5 +1,8 @@
 import java.rmi.Naming;
 
+/*
+ * Simple client asking server for 10 messages
+ */
 public class RmiClient {
     public static void main(String args[]) throws Exception {
         RmiServerIntf obj = (RmiServerIntf)Naming.lookup("//localhost/RmiServer");
